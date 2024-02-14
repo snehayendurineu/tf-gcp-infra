a23b66aaf953f6e128a4666ad50c992b510a395d
snehayendurineu <yenduri.s@northeastern.edu>
2024-02-13 22:25:05 -0500

    added terraform assg3 files

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "gcp_project" {
+  
+}
+variable "gcp_region" {
+  
+}
+variable "vpc_network_auto_create_subnets" {
+  
+}
+
+variable "vpc_network_name" {}
+variable "webapp_subnet_name" {}
+variable "webapp_subnet_cidr" {}
+variable "db_subnet_name" {}
+variable "db_subnet_cidr" {}
+
+variable "vpc_network_routing_mode" {
+  
+}
+variable "webapp_route_name" {
+  
+}
