2ea1ad007d24dfaf27fa4ce0f3f4745a60772189
snehayendurineu <yenduri.s@northeastern.edu>
2024-02-21 19:32:28 -0500

    Revert "added files"
    This reverts commit ccef048fd546803bcebd937fd1a92672a9d0823c.

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 # Create VPC network
 resource "google_compute_network" "vpc_main_network" {
-  name                            = var.vpc_network_name
-  auto_create_subnetworks         = var.vpc_network_auto_create_subnets
-  routing_mode                    = var.vpc_network_routing_mode
+  name = var.vpc_network_name
+  auto_create_subnetworks = var.vpc_network_auto_create_subnets
+  routing_mode = var.vpc_network_routing_mode
   delete_default_routes_on_create = var.vpc_network_delete_default_routes
 }
 
@@ -22,70 +22,11 @@ resource "google_compute_subnetwork" "db_subnet" {
 
 # Create route for webapp subnet
 resource "google_compute_route" "webapp_route" {
-  name             = var.webapp_route_name
-  network          = google_compute_network.vpc_main_network.self_link
-  dest_range       = "0.0.0.0/0"
-  next_hop_gateway = "default-internet-gateway"
-  priority         = 1000
-  tags             = [var.webapp_subnet_name]
+  name                  = var.webapp_route_name
+  network               = google_compute_network.vpc_main_network.self_link
+  dest_range            = "0.0.0.0/0"
+  next_hop_gateway      = "default-internet-gateway"
+  priority              = 1000
+  tags                  = [var.webapp_subnet_name]
 }
 
-resource "google_compute_firewall" "allow_http" {
-  name    = "allow-http"
-  network = google_compute_network.vpc_main_network.id
-
-  direction = "INGRESS"
-  priority  = 1000
-  disabled  = false
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-  target_tags = ["http-server"]
-}
-
-resource "google_compute_firewall" "deny_ssh" {
-  name    = "deny-ssh"
-  network = google_compute_network.vpc_main_network.id
-
-  direction = "INGRESS"
-  priority  = 65534
-  disabled  = false
-
-  deny {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-  target_tags = ["http-server"]
-}
-
-
-resource "google_compute_instance" "vm-instance-1" {
-  name         = "vm-instance-1"
-  machine_type = "e2-medium"
-  zone         = var.zone
-
-  boot_disk {
-    initialize_params {
-      image = var.cusimage_name
-      size  = 100
-      type  = "pd-balanced"
-    }
-  }
-  network_interface {
-    network = google_compute_network.vpc_main_network.self_link
-    subnetwork = google_compute_subnetwork.webapp_subnet.self_link
-    access_config {}
-  }
-
-  service_account {
-    email = "pkr-serv-acct@cloud6225-dev.iam.gserviceaccount.com"
-    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
-  }
-  tags = ["http-server"]
-}
--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "gcp_project" {
-
+  
 }
 variable "gcp_region" {
-
+  
 }
 variable "vpc_network_auto_create_subnets" {
-
+  
 }
 
 variable "vpc_network_name" {}
@@ -15,28 +15,12 @@ variable "db_subnet_name" {}
 variable "db_subnet_cidr" {}
 
 variable "vpc_network_routing_mode" {
-
+  
 }
 variable "webapp_route_name" {
-
+  
 }
 
 variable "vpc_network_delete_default_routes" {
-
-}
-
-variable "http_port" {
-  type    = string
-  default = "8080"
-}
-
-variable "cusimage_name" {
-  type    = string
-  default = "custom-cosimage"
-}
-
-
-variable "zone" {
-  type    = string
-  default = "us-east1-b"
+  
 }
