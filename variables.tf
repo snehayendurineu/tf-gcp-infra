68f8c5c456b073414e655158f6bcdd4d61fdc9d7
snehayendurineu <yenduri.s@northeastern.edu>
2024-02-27 15:50:03 -0500

    assg05

--- variables.tf
+++ variables.tf
@@ -2,10 +2,10 @@ variable "gcp_project" {
 
 }
 variable "gcp_region" {
-
+  type    = string
+  default = "us-east1"
 }
 variable "vpc_network_auto_create_subnets" {
-
 }
 
 variable "vpc_network_name" {}
@@ -15,10 +15,12 @@ variable "db_subnet_name" {}
 variable "db_subnet_cidr" {}
 
 variable "vpc_network_routing_mode" {
-
+  type    = string
+  default = "REGIONAL"
 }
 variable "webapp_route_name" {
-
+  type    = string
+  default = "webapp-route"
 }
 
 variable "vpc_network_delete_default_routes" {
@@ -32,7 +34,7 @@ variable "http_port" {
 
 variable "cusimage_name" {
   type    = string
-  default = "custom-cosimage"
+  default = "cusimage-nomysql"
 }
 
 
@@ -44,4 +46,36 @@ variable "zone" {
 variable "srv-acct-email" {
   type    = string
   default = "pkr-serv-acct@cloud6225-dev.iam.gserviceaccount.com"
+}
+
+
+variable "db-version" {
+  type    = string
+  default = "MYSQL_8_0"
+}
+
+
+variable "db_name" {
+  type    = string
+  default = "webapp"
+}
+
+variable "private_service_access_ip_name" {
+  type    = string
+  default = "global-psconnect-ip"
+}
+
+variable "db-availability-type" {
+  type    = string
+  default = "REGIONAL"
+}
+
+variable "database" {
+  type    = string
+  default = "webapp"
+}
+
+variable "db-user" {
+  type    = string
+  default = "webapp"
 }
